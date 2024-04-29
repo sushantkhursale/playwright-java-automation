441af52091c9023704dea40fdb76eee4887cdadc
10963-LAP-0563\Coditas <sushant.khursale@coditas.com>
2024-04-29 18:06:14 +0530

    Committing data driven test case

--- src/main/java/utilities/DataProviders.java
+++ src/main/java/utilities/DataProviders.java
@@ -8,31 +8,9 @@ public class DataProviders {
 
     private String fileName = "UserData.xlsx";
     private String sheetName = "Sheet1";
-//    public DataProviders(String fileName, String sheetName){
-//        this.fileName = fileName;
-//        this.sheetName = sheetName;
-//    }
-
-    @DataProvider(name="Data")
-    public String[][] getDataFromExcel() throws IOException {
-        String filePath = System.getProperty("user.dir")+"\\src\\test\\resources\\testData\\"+fileName;
-        ExcelUtility xlUtility = new ExcelUtility();
-        int noOfRows = xlUtility.getRowCount(filePath,sheetName);
-        int noOfColumns = xlUtility.getCellCount(filePath,sheetName,0);
-
-        String[][] excelDataArray = new String[noOfRows][noOfColumns];
-
-        for(int i=1;i<=noOfRows;i++){
-            for(int j=0;j<noOfColumns;j++){
-                excelDataArray[i-1][j] = xlUtility.getCellData(filePath,sheetName,i,j);
-            }
-
-        }
-        return excelDataArray;
-    }
 
 
-    @DataProvider(name="Usernames")
+    @DataProvider(name="options")
     public String[] getUsernames() throws IOException {
 
         String filePath = System.getProperty("user.dir")+"\\src\\test\\resources\\testData\\"+fileName;
--- src/test/java/tests/HomePageTests.java
+++ src/test/java/tests/HomePageTests.java
@@ -7,16 +7,16 @@ import com.microsoft.playwright.Page;
 import base.BaseTest;
 import pages.HomePage;
 import pages.LoginPage;
+import utilities.DataProviders;
 
 public class HomePageTests extends BaseTest {
 
 
 	/* Test to verify all top menu options are working fine
 	 */
-	@Test
-	public void clickTopMenuTests() throws InterruptedException {
-		homePage.clickTopMenuItem("Leaderboard");
-		
+	@Test(dataProvider = "options",dataProviderClass = DataProviders.class)
+	public void clickTopMenuTests(String option) throws InterruptedException {
+		homePage.clickTopMenuItem(option);
 	}
 
 }
